f85fd3047f2b74a7beae8e0843fe3e7796983067
Udita Bose <ub298@nyu.edu>
2017-07-04 17:37:00 -0400

    updated etcd config, some re-org

--- files/run-etcd-node.sh
+++ files/run-etcd-node.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #############################################################################
 # The MIT License (MIT)                                                     #
 #                                                                           #
@@ -23,28 +25,31 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                    #
 #############################################################################
 
-# this playbook tests liveness and connectivity of all hosts
----
 
-- name: Ping it!
-  sudo: True
-  hosts: remote
-  vars:
-    HOST_COUNT: "{{ groups['remote'] | length }}"
-    #HOST_VARS: |
-    #  {% for item in groups['remote'] -%}
-    #      {{ hostvars[item].ansible_default_ipv4.address }}
-    #  {%- endfor %}
-    HOST_VARS: "{{ groups['remote']|map('extract', hostvars, 'ansible_host') | join(' ') }}"
+echo ""
+echo "*********** RUN etcd Cluster : Start ************"
 
-  tasks :
-    - name: Ping it alright!
-      shell: ping -c 3 localhost
-      register: pinged
+ETCD_PATH="$1"
+infra_name="$2"
 
-    #- debug: var={{ hostvars[ansible_hostname] }}
-    - debug: var=HOST_VARS
+infra1="$3"
+infra2="$4"
+infra3="$5"
+if [[ $infra_name == 'infra0' ]]; then 
+	infra_ip=$infra1
+elif [[ $infra_name == 'infra1' ]]; then
+	infra_ip=$infra2
+elif [[ $infra_name == 'infra2' ]]; then
+	infra_ip=$infra3
+fi
 
+cluster="infra0=http://$infra1:2380,infra1=http://$infra2:2380,infra2=http://$infra3:2380"
+listen="http://$infra_ip:2379,http://127.0.0.1:2379"
 
-
-
+$ETCD_PATH/etcd --name $infra_name --initial-advertise-peer-urls "http://$infra_ip:2380" \
+--listen-peer-urls "http://$infra_ip:2380" \
+--listen-client-urls $listen \
+--advertise-client-urls "http://$infra_ip:2379" \
+--initial-cluster-token etcd-cluster-1 \
+--initial-cluster $cluster \
+--initial-cluster-state new
