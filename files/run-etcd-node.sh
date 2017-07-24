826c692bba00728b6190cc0f42dee8a2dedbe4ba
Udita Bose <ub298@nyu.edu>
2017-07-24 01:53:03 -0400

    kuberenetes setup

--- files/run-etcd-node.sh
+++ files/run-etcd-node.sh
@@ -29,27 +29,44 @@
 echo ""
 echo "*********** RUN etcd Cluster : Start ************"
 
-ETCD_PATH="$1"
-infra_name="$2"
+ETCD_PATH="$1" 			# ETCD bin path
+cluster_token="$2"		# cluster token
+infra_ip="$3"			# IP of this node
+shift 3					# move 3 spaces
+cluster_ips="$*"		# IPs of all nodes
 
-infra1="$3"
-infra2="$4"
-infra3="$5"
-if [[ $infra_name == 'infra0' ]]; then 
-	infra_ip=$infra1
-elif [[ $infra_name == 'infra1' ]]; then
-	infra_ip=$infra2
-elif [[ $infra_name == 'infra2' ]]; then
-	infra_ip=$infra3
-fi
+# create the cluster URL
+infra_name=""			# name of this node <infra_node_name>
+cluster=""				# cluster URL
+cluster_cnt=0			# cluster node number
 
-cluster="infra0=http://$infra1:2380,infra1=http://$infra2:2380,infra2=http://$infra3:2380"
+# the cluster string is like : 
+# "<infra_node_name>=http://<infra_node_ip>:2380,<infra_other_node_name>=http://<infra_other_node_ip>:2380"
+# this pattern repeats for as many nodes are there
+# all nodes have exactly same cluster string
+for ip in $cluster_ips; do
+	if [[ "$ip" == "$infra_ip" ]]; then
+		infra_name="infra$cluster_cnt"
+	fi
+	cluster+="infra$cluster_cnt=http://$ip:2380,"
+	cluster_cnt=$((cluster_cnt + 1))
+done
+
+cluster=${cluster:0:-1} # remove trailing comma(,)
+echo "etcd cluster : $cluster"
+
+# this node listens on this URL
 listen="http://$infra_ip:2379,http://127.0.0.1:2379"
 
-$ETCD_PATH/etcd --name $infra_name --initial-advertise-peer-urls "http://$infra_ip:2380" \
+# start the cluster
+$ETCD_PATH/etcd --name "$infra_name" \
+--initial-advertise-peer-urls "http://$infra_ip:2380" \
 --listen-peer-urls "http://$infra_ip:2380" \
---listen-client-urls $listen \
+--listen-client-urls "$listen" \
 --advertise-client-urls "http://$infra_ip:2379" \
---initial-cluster-token etcd-cluster-1 \
---initial-cluster $cluster \
+--initial-cluster-token "$cluster_token" \
+--initial-cluster "$cluster" \
 --initial-cluster-state new
+
+echo ""
+echo "*********** RUN etcd Cluster : Finish ************"
